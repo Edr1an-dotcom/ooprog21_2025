27b521e1062ebb6580a2ee5f85db9d76b13192db
JohnEdrianDiaz-UCM <edriandiaz45@gmail.com>
2025-09-23 08:21:03 +0800

    Update DebugBox.java

--- Chapter_4/DebugBox.java
+++ Chapter_4/DebugBox.java
@@ -1,6 +1,6 @@
 class DebugBox {
    public static void main(String[] args) {
-      // Create an instance using the default constructor
+
       DebugBox box = new DebugBox(); 
       box.showData(); // Call showData() without printing its return value
    }
@@ -9,28 +9,25 @@ class DebugBox {
    private int length;
    private int height;
 
-   // Default constructor with default values
    public DebugBox() {
       length = 1;
       width = 1;
       height = 1;
    }
 
-   // Parameterized constructor for custom dimensions
    public DebugBox(int width, int length, int height) {
       this.width = width;
       this.length = length;
       this.height = height;
    }
 
-   // Method to calculate volume
    public double getVolume() { 
       return length * width * height;
    }
-
-   // Method to display box data
+   
    public void showData() {
       System.out.println("Width: " + width + "  Length: " + length + 
                          "  Height: " + height + "  Volume: " + getVolume());
    }
 }
+
